8a138006c48337390e07516a004e0845f7f71c5c
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-04-03 08:03:20 +0530

    updated content script

--- scripts/content.js
+++ scripts/content.js
@@ -3,30 +3,14 @@ const attendance_display = document.querySelector(
   "#js-page-content > div:nth-child(6) > div.col-xl-12"
 );
 
-// `document.querySelector` may return null if the selector doesn't match anything.
+// attendance link in the dashboard
 if (attendance_link) {
-  console.log(attendance_link);
   attendance_link.style.display = "block";
-  //   const text = article.textContent;
-  //   const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  //   const words = text.matchAll(wordMatchRegExp);
-  //   // matchAll returns an iterator, convert to array to get word count
-  //   const wordCount = [...words].length;
-  //   const readingTime = Math.round(wordCount / 200);
-  //   const badge = document.createElement("p");
-  //   // Use the same styling as the publish information in an article's header
-  //   badge.classList.add("color-secondary-text", "type--caption");
-  //   badge.textContent = `⏱️ ${readingTime} min read`;
-
-  //   // Support for API reference docs
-  //   const heading = article.querySelector("h1");
-  //   // Support for article docs with date
-  //   const date = article.querySelector("time")?.parentNode;
-
-  //   (date ?? heading).insertAdjacentElement("afterend", badge);
+  console.log("NIET Attendance: Attendance link is visible");
 }
 
+// main attendance display
 if (attendance_display) {
-  console.log(attendance_display);
   attendance_display.style.display = "block";
+  console.log("NIET Attendance: Attendance is visible");
 }
