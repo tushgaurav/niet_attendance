9d8a502d26df9ea09fc5b30f3f2541d6b21a68d8
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-04-14 15:19:25 +0530

    fixed bugs and code improvement
    - fixed the attendance table header full width bug
    - updated the directory structure
    - updated the onboarding page

--- scripts/content.js
+++ scripts/content.js
@@ -68,3 +68,16 @@ const cover_image = document.querySelector(
 
 cover_image.src = chrome.runtime.getURL("/assets/images/background.gif");
 cover_image.classList.add("img-thumbnail");
+
+// fixing the attendance table header widht
+let table_container = document.querySelector(
+  "#tblDetails1_wrapper > div:nth-child(2) > div > div > div.dataTables_scrollHead > div"
+);
+
+table_container.style.width = "100%";
+
+let table_head = document.querySelector(
+  "#tblDetails1_wrapper > div:nth-child(2) > div > div > div.dataTables_scrollHead > div > table"
+);
+
+table_head.style.width = "100%";
